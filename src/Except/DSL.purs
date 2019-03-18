fec37708afa4ee08865604f36c8288627cdb701e
aravind mallapureddy <aravind.mallapureddy@juspay.in>
2019-03-18 17:14:47 +0530

    added handler functions

--- src/Except/DSL.purs
+++ src/Except/DSL.purs
@@ -3,28 +3,28 @@ module Except.Dsl where
 import Control.Monad.Free (Free, liftF)
 import Data.Maybe (Maybe)
 import Effect.Aff (Aff)
-import Prelude (Unit, ($), (<<<))
+import Prelude (Unit, identity, ($))
 
 
 
-data FlowE st a =  Command String (Array String)
-               | When String (Maybe String)
-               | Respond String
-               | End (String -> Aff Unit)
+data FlowE st a =  Command String (Array String) (Unit -> a)
+               | When String (Maybe String) (Unit -> a)
+               | Respond String (Unit -> a)
+               | End (String -> Aff Unit) (Unit -> a)
 
 
 type ExceptFlow st a = Free (FlowE st) a
 
 
 command :: forall st. String -> Array String -> ExceptFlow st Unit 
-command cmd options = liftF $ Command cmd options
+command cmd options = liftF $ Command cmd options identity
 
 when :: forall st. String -> Maybe String -> ExceptFlow st Unit
-when str = liftF <<< When str
+when str maybeResp = liftF $ When str maybeResp identity
 
 respond :: forall st. String -> ExceptFlow st Unit
-respond = liftF <<< Respond
+respond resp = liftF $ Respond resp identity
 
 end :: forall st. (String -> Aff Unit) -> ExceptFlow st Unit
-end = liftF <<< End
+end f = liftF $ End f identity
 
--- src/Except/Runner.purs
+++ src/Except/Runner.purs
@@ -3,21 +3,34 @@ module Except.Runner where
 
 import Control.Monad.Except.Trans (ExceptT)
 import Control.Monad.Free (foldFree)
+import Control.Monad.State (modify_)
 import Control.Monad.State.Trans (StateT)
+import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple(..))
 import Effect.Aff (Aff)
 import Effect.Exception (Error)
 import Except.Dsl (ExceptFlow, FlowE(..))
-import Prelude (unit)
+import Foreign.Object (Object, insert, pop)
+import Prelude (pure, unit, (<<<), (>>=))
 import Unsafe.Coerce (unsafeCoerce)
 
 
-type Runner st e a = StateT st (ExceptT e Aff) a
+type Runner st e a = StateT (Object String) (ExceptT e Aff) a
 
 run :: forall st a. ExceptFlow st a -> Runner st Error a
 run flow = foldFree (\f -> interpret f) flow
 
 interpret :: forall st a. FlowE st a -> Runner st Error a
-interpret (Command cmd options) = unsafeCoerce unit
-interpret (When str maybeResp) = unsafeCoerce unit
-interpret (Respond resp) = unsafeCoerce unit
-interpret (End f) = unsafeCoerce unit
+interpret (Command cmd options next) = unsafeCoerce unit
+interpret (When str (Just resp) next) = modify_ (\st -> insert str resp st) >>= (pure <<< next) 
+interpret (When str Nothing next) = modify_ (\st -> insert "lastwhen" str st) >>= (pure <<< next)
+interpret (Respond resp next) = modify_ (handler resp) >>= (pure <<< next) 
+interpret (End f next) = unsafeCoerce unit
+
+
+
+handler :: String -> Object String -> Object String
+handler resp st = do
+    case pop "lastwhen" st of
+         Just (Tuple key map) -> insert key resp map
+         Nothing -> st
